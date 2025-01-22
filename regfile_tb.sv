61edd2f2a3f3906ffbfbebfc913d63df49a6e968
Holland Carter <44069863+deserthobo13@users.noreply.github.com>
2025-01-22 13:57:10 -0600

    Update regfile_tb.sv

--- regfile_tb.sv
+++ regfile_tb.sv
@@ -1,20 +1,20 @@
 module stimulus ();
 
    logic   clock;
-   logic	we3;
-   logic	ra1;
-   logic	ra2;
-   logic	wa3;
-   logic	wd3;
+   logic 	we3;
+   logic [4:0]	ra1;
+   logic [4:0]	ra2;
+   logic [4:0]	wa3;
+   logic [31:0]	wd3;
    
-   logic rd1;
-   logic rd2;
+   logic [31:0] rd1;
+   logic [31:0] rd2;
    
    integer handle3;
    integer desc3;
    
    // Instantiate DUT
-   fsm dut (clock, we3, ra1, ra2, wa3, wd3, rd1, rd2);
+   regfile dut (clock, we3, ra1, ra2, wa3, wd3, rd1, rd2);
 
    // Setup the clock to toggle every 1 time units 
    initial 
@@ -34,8 +34,8 @@ module stimulus ();
    always 
      begin
 	desc3 = handle3;
-	#5 $fdisplay(desc3, "%b %b || %b", 
-		     reset, In, Out);
+	#5 $fdisplay(desc3, "%b %b %b %b %b %b %b", 
+		     we3, ra1, ra2, wa3, wd3, rd1, rd2);
      end   
    
    initial 
@@ -47,8 +47,6 @@ module stimulus ();
 	#20 we3 = 1'b1;
 	#10 wd3 = 32'b0;
 	
-	#20 rd1 = 32'b0;
-	#0  rd2 = 32'b0;
 	#10 ra1 = 5'b10110;
 	#10 ra2 = 5'b00111;
 	#20 ra1 = 5'b0;
